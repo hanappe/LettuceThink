a5b208a0596c65b93a845345259edc45b39ffd03
hanappe <peter@hanappe.com>
2017-03-30 12:31:27 +0200

    Changes for stepper with 15.3 gearbox ratio

--- LettuceMove2/WheelModule/WheelModuleProMini/WheelModuleProMini.ino
+++ LettuceMove2/WheelModule/WheelModuleProMini/WheelModuleProMini.ino
@@ -14,15 +14,16 @@ AccelStepper stepper(AccelStepper::DRIVER,  DIRECTION_STEP,  DIRECTION_DIR);
 
 void setup()
 { 
-  //Serial.begin(115200);
+  Serial.begin(115200);
 #if STEPPER_15
-  stepper.setMaxSpeed(5000);
+  stepper.setMaxSpeed(4000);
+  stepper.setAcceleration(2500);
 #elif STEPPER_77
   stepper.setMaxSpeed(5000);
+  stepper.setAcceleration(3000);
 #else 
 #error Please define the stepper
 #endif
-  stepper.setAcceleration(3000);
 
   pinMode(encoder0PinA, INPUT); 
   digitalWrite(encoder0PinA, HIGH);       // turn on pull-up resistor
@@ -52,15 +53,20 @@ unsigned long lastRead = 0;
 void loop()
 {
   unsigned long now = millis();
-  if (now - lastRead > 20) {
+  if (now - lastRead > 10) {
     lastRead = now;
     int targetAngle = map(analogRead(A0), 0, 1023, -90, 90);
     if (abs(targetAngle - currentAngle) > 1) {
-      long pos = map(targetAngle, -90, 90, -MAX_STEPS, MAX_STEPS);
-      stepper.moveTo(pos);
-//      Serial.print(targetAngle);
-//      Serial.print(' ');
-//      Serial.println(currentAngle);
+      long current = stepper.currentPosition();
+      long delta = map(targetAngle - currentAngle, -90, 90, -MAX_STEPS, MAX_STEPS);
+      stepper.moveTo(current + delta);
+      /*Serial.print(targetAngle);
+      Serial.print(' ');
+      Serial.print(currentAngle);
+      Serial.print(' ');
+      Serial.print(current);
+      Serial.print(' ');
+      Serial.println(delta);*/
     }
   }
   stepper.run();
@@ -75,10 +81,15 @@ SIGNAL(INT0_vect)
     encoder0Pos++;    //encoder is moving forward
   else
     encoder0Pos--;    //encoder is moving backward
+#if STEPPER_15
+  currentAngle = (int) (36 * encoder0Pos / 459); // pos*360/300/15.3
+#elif STEPPER_77
   // 1 encoder step corresponds to 360/300 degrees on the stepper axis 
   // 1 degree on the stepper axis corresponds to 1/(76+49/64) degrees on the wheel axis, or 1/76.765625 degrees
   // => 1 encoder step corresponds to 360/300/76.765625 degrees on the wheel axis, or approx. 360/23030 degrees
-  currentAngle = (int) (360 * encoder0Pos / 23030); // pos*360/300/76.765625
+#else 
+#error Please define the stepper
+#endif
 }
 
 
